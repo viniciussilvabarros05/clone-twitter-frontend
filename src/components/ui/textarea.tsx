6f80099b74ae5765d4f8e0e492d418f24b3ea2ae
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2024-09-19 12:19:49 -0300

    feat: add new component of textarea

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  placeholder: string;
+  rows: number;
+  value?: string;
+};
+export const Textarea = ({ placeholder, rows, value }: Props) => {
+  return (
+    <div className="has-[:focus]:border-white flex items-center rounded-3xl border-2 border-gray-700">
+      <textarea
+        className="flex-1 outline-none bg-transparent h-full p-5 resize-none"
+        id=""
+        rows={rows}
+        placeholder={placeholder}
+        value={value}
+      ></textarea>
+    </div>
+  );
+};
